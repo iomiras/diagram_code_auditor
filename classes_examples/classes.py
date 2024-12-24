0847e300cb28ed67a755e3a9d2b209366d4ebff4
iomiras <miras.shaltayev@gmail.com>
2024-12-24 17:15:13 +0500

    test

--- classes_examples/classes.py
+++ classes_examples/classes.py
@@ -167,12 +167,12 @@ class AuthServer:
     def lorem_ipsum_auth(self):
         print(f"{self.name} has no store_data method.")
 
-# class Demo:
-#     def __init__(self, name):
-#         self.name = name
+class Demo:
+    def __init__(self, name):
+        self.name = name
 
-#     def demo_method(self):
-#         print(f"{self.name} has no store_data method")
+    def demo_method(self):
+        print(f"{self.name} has no store_data method")
     
-#     def lorem_ipsum(self):
-#         print(f"{self.name} has no store_data methods.")
+    def lorem_ipsum(self):
+        print(f"{self.name} has no store_data methods.")
